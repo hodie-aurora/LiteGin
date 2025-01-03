d4d700214d90216e2a476faf109f4c9054133995
赵海雯 <1726195788@qq.com>
2025-01-03 15:53:25 +0800

    Static Route Resolution

--- lg/lg.go
+++ lg/lg.go
@@ -2,8 +2,51 @@ package lg
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
-func Test() {
-	fmt.Println("test")
+// HandlerFunc defines the request handler used by lg
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+// Engine implements the interface of ServeHTTP
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+// New is the constructor of Engine
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+// addRoute adds a new route to the router
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	log.Printf("Router is %s - %s", method, pattern)
+	engine.router[key] = handler
+}
+
+// GET adds a GET request handler to the router
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST adds a POST request handler to the router
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run starts the HTTP server on the specified address
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+// ServeHTTP handles incoming HTTP requests
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s %s\n", req.Method, req.URL)
+	}
 }
--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"lg"
+	"net/http"
 )
 
 func main() {
-	lg.Test()
+	r := lg.New()
+	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("hello, World!"))
+	})
+	r.GET("/hi", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("hi, World!"))
+	})
+	r.POST("/BAZINGA", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("BAZINGA, World!"))
+	})
+	r.Run(":7777")
 }
